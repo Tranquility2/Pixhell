54826ec456e769b445281d5e7500c2341e822231
Roy Moore <roy@moore.co.il>
2020-03-29 23:29:04 +0300

    test rectangle

--- pixhell/src/main.cpp
+++ pixhell/src/main.cpp
@@ -16,9 +16,21 @@ int main(int argc, char *argv[])
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
   SDL_RenderClear(renderer);
+
+  SDL_Rect rect;
+  rect.x = 250;
+  rect.y = 150;
+  rect.w = 200;
+  rect.h = 200;
+  
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderFillRect(renderer, &rect);
+  
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
   SDL_RenderPresent(renderer);
 
-  SDL_Delay(3000);
+  SDL_Delay(10000);
 
   SDL_DestroyWindow(window);
   SDL_Quit();
